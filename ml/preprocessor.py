95b69df5bd60784bfb7dd9db116f43c92a6fd1e5
eugen <eugeny.pakhalyuk@yandex.ru>
2022-08-26 18:39:31 +0300

    add tasks

--- ml/preprocessor.py
+++ ml/preprocessor.py
@@ -1,4 +1,8 @@
 import spacy
+from functools import lru_cache
+
+# pip install spacy==3.1.0
+# python -m spacy download ru_core_news_lg
 
 
 class Preprocessor(object):
@@ -8,5 +12,14 @@ class Preprocessor(object):
     def light_preprocess(self, text):
         pass
 
+    @lru_cache(10**10)
+    def lemmatizate(self, word):
+        return self.nlp(word)[0].lemma_
+
     def hard_preprocess(self, text):
+        """Только кириллица, слова лемматизированы, все числа заменены токеном <num>, без стоп слов"""
         pass
+
+
+p = Preprocessor()
+print(p.lemmatizate("ноутбуком"))
