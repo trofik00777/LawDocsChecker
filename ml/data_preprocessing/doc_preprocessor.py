1648113f569beb16323c5c713ea88687507d7447
Maxim Trofimov <maks.trofimov.0303@mail.ru>
2022-08-27 18:03:52 +0300

    create doc parsing by brackets

--- ml/data_preprocessing/doc_preprocessor.py
+++ ml/data_preprocessing/doc_preprocessor.py
@@ -2,6 +2,8 @@ import re
 from string import punctuation
 from typing import List
 
+from loguru import logger
+
 from docx import Document
 
 import nltk
@@ -14,20 +16,47 @@ russian_stopwords = stopwords.words("russian")
 patterns = re.compile(r"[A-Za-z0-9!#$%&'()*+,./:;<=>?@[\]^_`{|}~—\"\-]+")
 split_regex = re.compile(r'[.|!|?|…]')
 
+COUNT_POSSIBLE_CHARS_BETWEEN_BRACKETS = 2
+
 
 class DocProcessor:
-    @classmethod
-    def preprocess_doc(cls, doc: Document) -> List[str]:
+    @staticmethod
+    def get_full_text(doc: Document):
         full_text = []
 
         for para in doc.paragraphs:
             full_text.append(para.text)
 
-        text = ''.join(full_text)
-        sentences = filter(lambda t: t, [t.strip() for t in split_regex.split(text)])
+        return ' '.join(full_text)
+
+    @classmethod
+    def preprocess_doc_splitted_by_sentences(cls, doc: Document) -> List[str]:
+        sentences = filter(lambda t: t, [t.strip() for t in split_regex.split(cls.get_full_text(doc))])
         sentences = list(map(cls._preprocess_text, sentences))
         sentences = list(filter(lambda elem: not (elem is None), sentences))
         return sentences
+
+    @classmethod
+    def preprocess_doc_splitted_by_brackets(cls, doc: Document) -> List[str]:
+        text = cls.get_full_text(doc)
+        is_right = False
+        left_bracket = right_bracket = 0
+        paragraphs = []
+        for i in range(len(text)):
+            if text[i] == "{":
+                last_i = i
+                try:
+                    last_i = i + text[i:i + COUNT_POSSIBLE_CHARS_BETWEEN_BRACKETS + 2].index("}")
+                except Exception as e:
+                    logger.error(f"There are no bracket at '{text[i:i + 10]}'")
+
+                if is_right:
+                    right_bracket = i
+                    paragraphs.append(text[left_bracket + 1:right_bracket])
+                else:
+                    left_bracket = last_i
+                is_right = not is_right
+        return paragraphs
 
     @staticmethod
     def _preprocess_text(text: str) -> str:
@@ -44,3 +73,8 @@ class DocProcessor:
         text = " ".join(tokens)
         if len(tokens) > 2:
             return text
+
+
+if __name__ == "__main__":
+    file = Document("/Users/trofik00777/Documents/parser/helper/docs/Postanovlenie_Pravitelstva_Rossiyskoy_Federatsii_ot_04_03_2021__314.docx")
+    print(DocProcessor.preprocess_doc_splitted_by_brackets(file))
